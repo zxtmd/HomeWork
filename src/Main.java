c3654248f24bb193b70f714b1af77d429b6e7001
Aktan Kayirdinov <aktankayirdinov03@gmail.com>
2024-01-25 20:51:20 +0600

    added cat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Animal is sleeping");
         System.out.println(animal.isSleeping);
+
+    Cat cat = new Cat("Cat is sleeping");
+        System.out.println(cat.isSleeping);
     }
 }
