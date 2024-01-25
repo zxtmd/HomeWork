60671b384a58c069bbbd7483fdcaf5485e69c6b5
Aktan Kayirdinov <aktankayirdinov03@gmail.com>
2024-01-25 20:53:08 +0600

    added dog

---
+++ src/Dog.java
@@ -0,0 +1,5 @@
+public class Dog extends Animal{
+    public Dog(String isSleeping) {
+        super(isSleeping);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Animal is sleeping");
         System.out.println(animal.isSleeping);
+
+        Dog dog = new Dog("Dog is sleeping");
+        System.out.println(dog.isSleeping);
     }
 }
